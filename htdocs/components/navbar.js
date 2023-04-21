1290cc517746cd039482e7fb9b9f8a2bb14af8fc
massylamarni <111659560+massylamarni@users.noreply.github.com>
2023-04-21 07:48:35 +0100

    Delete htdocs directory

--- htdocs/components/navbar.js
+++
@@ -1,5 +0,0 @@
-function loginswitch()
-{
-	document.getElementsByClassName("navbar_auth")[0].style.display = "none";
-	document.getElementsByClassName("navbar_loggedin")[0].style.display = "flex";
-}
