b979fa3b9d0c405f2a8ca1450b5e343cf2ff7a46
YacineAitKaci <131439400+YacineAitKaci@users.noreply.github.com>
2023-04-29 11:49:16 +0000

    modified:   htdocs/components/navbar.php renamed:    logout.php -> htdocs/logout.php

--- htdocs/components/navbar.php
+++ htdocs/components/navbar.php
@@ -22,7 +22,7 @@ if(isset($_SESSION["usertype"]))
 	
 	<div class="navbar_auth">
 <a href="editprofile.php" class="navbar_loggedin"><img src="<?php echo $user_image ?>"/></a>
-<a href="editprofile.php" ><span><?php echo $name ?></span></a>
+<a href="editprofile.php" ><span><?php echo $name ?></span></a> ||
 <a href="logout.php" ><span>Logout</span></a>
    </div>
 
