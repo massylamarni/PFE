9f35643d21b66b18b6d3e7281c3d8d58e69eba20
YacineAitKaci <yacinej86@gmail.com>
2023-05-02 13:25:40 +0200

    modified:   v0.5_fix/editprofile.php

--- v0.5_fix/editprofile.php
+++ v0.5_fix/editprofile.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,173 +10,138 @@
 <body>
 
 <?php include("components/navbar.php");
+
+define("DB_NAME","Client");
+
 if(!isset($_SESSION))
 {
 session_start();
 }
-if(isset($_SESSION["usertype"]))  {
+if(isset($_SESSION["usertype"])) {
 
 	$conn = mysqli_connect('localhost', 'root', '', DB_NAME);
 
 	if($conn->connect_error){
 		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-        $email= $_SESSION["email"];
-	    $name=$_SESSION["name"];
-	    $bday=$_SESSION["bday"];
-      $phone=$_SESSION["phone"];
-	    $gender=$_SESSION["gender"]; 
-		  $pf_img = "assets/pfp2.png";
+     die("Connection Failed : ". $conn->connect_error);
+	 }else { 
+
+      $old_email= $_SESSION["email"];
+	    $old_name=$_SESSION["name"];
+      $old_password=$_SESSION["password"];
+	    $old_bday=$_SESSION["bday"];
+      $old_phone=$_SESSION["phone"];
+      @$old_location=$_SESSION["location"];
+		  $old_pf_img = "assets/pfp2.png";
+
+      if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        
+        $new_name=$_POST["name"];
+        $new_email=$_POST['email'];
+        $confirm_password=$_POST['old_password'];
+        $new_password = password_hash($_POST['new_password'], PASSWORD_DEFAULT);
+        $new_phone=$_POST["phone"];
+        $new_bday=$_POST["bday"];
+        $new_location=$_POST["location"];
+         
+        if ($new_email){
+          $stmt = $conn->prepare("SELECT email FROM patient WHERE email = ? UNION SELECT email FROM doctor WHERE email = ?");
+          $stmt->bind_param("ss", $new_email , $new_email);
+          $stmt->execute();
+          $result = $stmt->get_result();
+          if ( $new_email != $old_email  &&  $result->num_rows > 0) {
+            echo "email already exists";
+
+        }else{
+          $stmt = $conn->prepare("UPDATE patient SET  email = ? WHERE id = ?");
+          $stmt->bind_param("si", $new_email, $_SESSION["id"]);
+          $stmt->execute(); 
+          $_SESSION["email"]= $new_email;
+        }       
+			}
+         if( $new_name  && $new_name!=$old_name ){
+          $stmt = $conn->prepare("UPDATE patient SET name = ? WHERE id = ?");
+          $stmt->bind_param("si", $new_name, $_SESSION["id"]);
+          $stmt->execute();
+          $_SESSION["name"]=$new_name;
+         }
+
+         if($new_phone && $new_phone!=$old_phone ){
+          $stmt = $conn->prepare("UPDATE patient SET phone = ? WHERE id = ?");
+          $stmt->bind_param("si", $new_phone, $_SESSION["id"]);
+          $stmt->execute();
+          $_SESSION["phone"]=$new_phone;
+         }
+          if($new_bday  && $new_bday!=$old_bday ){
+          $stmt = $conn->prepare("UPDATE patient SET bday = ? WHERE id = ?");
+          $stmt->bind_param("si", $new_bday, $_SESSION["id"]);
+          $stmt->execute();
+          $_SESSION["bday"]=$new_bday;
+         }
+         if($new_location && $new_location!=$old_location ){
+          $stmt = $conn->prepare("UPDATE patient SET location = ? WHERE id = ?");
+          $stmt->bind_param("si", $new_location, $_SESSION["id"]);
+          $stmt->execute();
+          $_SESSION["location"]=$new_location;
+         }
+         if ($new_password && $confirm_password){
+             if(password_verify($confirm_password,$old_password)) {
+                $stmt = $conn->prepare("UPDATE patient SET password = ? WHERE id = ?");
+                $stmt->bind_param("si",$new_password , $_SESSION["id"]);
+                $stmt->execute();
+                $_SESSION["password"]=$new_password;
+        } 
+      
+      }
+         //header("Location: components/profile.php");
+         //exit(); 
+         
+         
+         header("Location: index.php");
+         exit();
+        }   
+      
+}
+
+ 
+   ?>
 
 
-  // get user input
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $avatar = $_FILES['avatar'];
 
-  // sanitize user input
-  $name = filter_var($name, FILTER_SANITIZE_STRING);
-  $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-  $password = filter_var($password, FILTER_SANITIZE_STRING);
-
-  // validate user input
-  $errors = array();
-
-  // check name
-  if (empty($name)) {
-    $errors[] = "Name is required";
-  }
-
-  // check email
-  if (empty($email)) {
-    $errors[] = "Email is required";
-  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors[] = "Email is invalid";
-  }
-
-  // check password
-  if (!empty($password)) {
-    // hash password
-    $password = password_hash($password, PASSWORD_DEFAULT);
-  } else {
-    // use old password
-    $sql = "SELECT password FROM users WHERE id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $_SESSION['user_id']);
-    $stmt->execute();
-    $stmt->bind_result($password);
-    $stmt->fetch();
-    $stmt->close();
-  }
-
-  // check avatar
-  if (!empty($avatar['name'])) {
-    // get file info
-    $filename = basename($avatar['name']);
-    $filetype = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
-    $filesize = $avatar['size'];
-    $filetmp = $avatar['tmp_name'];
-
-    // check file type
-    if ($filetype != "jpg" && $filetype != "png" && $filetype != "gif") {
-      $errors[] = "Only JPG, PNG and GIF files are allowed";
-    }
-
-    // check file size
-    if ($filesize > 500000) {
-      $errors[] = "File size must be less than 500 KB";
-    }
-
-    // move file to uploads folder
-    if (empty($errors)) {
-      move_uploaded_file($filetmp, "uploads/" . $filename);
-      // set avatar path
-      $avatar = "uploads/" . $filename;
-    }
-  } else {
-    // use old avatar
-    $sql = "SELECT avatar FROM users WHERE id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $_SESSION['user_id']);
-    $stmt->execute();
-    $stmt->bind_result($avatar);
-    $stmt->fetch();
-    $stmt->close();
-  }
-
-  // update user profile data
-  if (empty($errors)) {
-    // prepare sql query
-    $sql = "UPDATE users SET name = ?, email = ?, password = ?, avatar = ? WHERE id = ?";
-
-    // create prepared statement
-    $stmt = $conn->prepare($sql);
-
-    // bind parameters
-    $stmt->bind_param("ssssi", $name, $email, $password, $avatar, $_SESSION['user_id']);
-
-    // execute statement
-    if ($stmt->execute()) {
-      // update session variables
-      $_SESSION['user_name'] = $name;
-      $_SESSION['user_email'] = $email;
-      $_SESSION['user_avatar'] = $avatar;
-
-      // redirect to profile page
-      header("Location: profile.php");
-      exit();  }}
-    }?>
 
 <div class="std_container">
-	<div class="ep_container">
-		<?php //include("components/profile_blank.php"); ?>
-	</div>
-</div>
-
+	<div class="ep_container">		
 <h3>Gerer Compte</h3>
-<form class="ep_form">
-<div class="pf" id="<?php echo $id ?>">
+<form class="ep_form" method="POST">
+<div class="pf" id="<?php echo $_SESSION["id"] ?>">
 	<div class="pf_header">
-		<img src="<?php echo $pf_img ?>">
+		<img src="<?php echo $old_pf_img ?>"/>
 		<div class="pf_header_text">
-			<div class="pf_header_text_name"><textarea rows="1" cols="50"><?php echo $name ?></textarea></div>
-			<div class="pf_header_text_id"><?php echo $id ?></div>
+    <div class="pf_body_field"><h3>Nom</h3><input type="text" placeholder="<?php echo $old_name ?>" name="name" autocomplete="off"/></div>
 		</div>
 	</div>
 	<div class="pf_body">
-		<div class="pf_body_field"><h3>Numero telephone</h3><textarea rows="1" cols="15"><?php echo $phone ?></textarea></div>
-		<div class="pf_body_field"><h3>Adresse</h3><textarea rows="1" cols="50"><?php echo $location ?></textarea></div>
-		<div class="pf_body_field"><h3>Date Naissance</h3><input type="date"></div>
-		<div class="pf_body_field">
-			<h3>Genre</h3>
-			<div class="in_radio">
-				<div>
-					<input type="radio" id="r_male" name="gender" value="male">
-					<label for="r_male">Male</label>
-				</div>
-				<div>
-					<input type="radio" id="r_female" name="gender" value="female">
-					<label for="r_female">Female</label>
-				</div>
-			</div>
-		</div>
+  <div class="pf_body_field"><h3>Email</h3><input class="in_text" type="text"placeholder="<?php echo $old_email ?>" name="email" autocomplete="off"/></div>
+		<div class="pf_body_field"><h3>Date Naissance</h3><input type="date" name="bday"></div>
 	</div>
 	<div>
-		<div class="pf_body_field">
-			<label>Email</label>
-			<input class="in_text" type="text">
-		</div>
-		<div class="pf_body_field">
-			<label>Mot Passe</label>
-			<input class="in_text" type="text">
-		</div>
+  <div class="pf_body_field"><h3>Numero telephone</h3><input type="text" placeholder="<?php echo $old_phone ?>" name="phone"  autocomplete="off"/></div>
+  <div class="pf_body_field"><h3>Location</h3><input type="text" placeholder="<?php  @$old_location ?>" name="location" autocomplete="off" /></div>
+		<div class="pf_body_field"><h3>Password</h3><input class="in_text" type="password" placeholder="enter old password" name="old_password" autocomplete="off">
+    <input class="in_text" type="password" placeholder="enter new password" name="new_password" autocomplete="off" ></div>
 	</div>
 </div>
-<input type="submit">
+<input type="submit" value="modifier">
 </form>
+</div>
+</div>
 
 <script src="index.js"></script>
 </body>
-</html>
+</html> 
+
+<?php   }else{
+
+  header("Location: index.php");
+  exit();
+}  ?>
