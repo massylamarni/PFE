832193445194df00fe11051bbe2407840195228a
YacineAitKaci <yacinej86@gmail.com>
2023-05-03 18:05:35 +0100

    modified:   v0.5_fix/components/navbar.php

--- v0.5_fix/components/navbar.php
+++ v0.5_fix/components/navbar.php
@@ -1,7 +1,16 @@
 <div class="navbar">
 	<div class="prenav"></div>
 	<div class="navbar_el_container">
-	<?php
+	<?php /*
+	$current_file_name= basename($_SERVER["PHP_SELF"]);
+	if ($current_file_name == "login.php" || $current_file_name == "signup.php" || $current_file_name == "PS_signup.php") {
+
+?>
+		<a href="index.php" class="navbar_logo"><img src="assets/logo.png"></a>
+<?php 
+}else{*/
+
+	
 if(!isset($_SESSION))
 {
 session_start();
@@ -11,33 +20,19 @@ if(isset($_SESSION["usertype"])) {
 	$name = $_SESSION['name'];
 	$user_image = "assets/pfp6.png";
 
-	if ($_SESSION["usertype"]=='patient'){?>
+		$logo_url = ($_SESSION["usertype"] == 'doctor') ? 'PS_index.php' : 'index.php';
+		$edit_profile_url = ($_SESSION["usertype"] == 'doctor') ? 'PS_editprofile.php' : 'editprofile.php';		
+		?>
 
-		<a href="index.php" class="navbar_logo"><img src="assets/logo.png"></a>
+		<a href=<?php echo $logo_url  ?> class="navbar_logo"><img src="assets/logo.png"></a>
 		<form id="navbar_search" action="resultlist.php">
 			<input type="text" class="navbar_search_location" placeholder="Emplacement...">
 			<input type="text" class="navbar_search_speciality" placeholder="Specialité...">
 			<input type="submit" class="navbar_search_submit" value="Rechercher">
 		</form>	
 		<div class="navbar_auth">
-	        <a href="editprofile.php" class="navbar_loggedin"><img src="<?php echo $user_image ?>"/><span><?php echo $name ?></span></a>||<a href="logout.php"><span>Logout</span></a>
-        </div>
-
-<?php }elseif($_SESSION["usertype"]=='doctor') { ?>
-
-        <a href="PS_index.php" class="navbar_logo"><img src="assets/logo.png"></a>
-		<form id="navbar_search" action="resultlist.php">
-			<input type="text" class="navbar_search_location" placeholder="Emplacement...">
-			<input type="text" class="navbar_search_speciality" placeholder="Specialité...">
-			<input type="submit" class="navbar_search_submit" value="Rechercher">
-		</form>	
-		<div class="navbar_auth">
-	        <a href="PS_editprofile.php" class="navbar_loggedin"><img src="<?php echo $user_image ?>"/><span><?php echo $name ?></span></a>||<a href="logout.php"><span>Logout</span></a>
-        </div>
-
-
-<?php } ?> 
-					
+	        <a href=<?php echo $edit_profile_url  ?> class="navbar_loggedin"><img src="<?php echo $user_image ?>"/><span><?php echo $name ?></span></a>||<a href="logout.php"><span>Logout</span></a>
+        </div>					
 </div>
 </div>
 
