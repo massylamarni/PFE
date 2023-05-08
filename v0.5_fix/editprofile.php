8e34e4d649490d471209af9faa383e001cc9a598
YacineAitKaci <yacinej86@gmail.com>
2023-05-08 23:11:51 +0100

    modified:   v0.5_fix/PS_editprofile.php 	modified:   v0.5_fix/editprofile.php

--- v0.5_fix/editprofile.php
+++ v0.5_fix/editprofile.php
@@ -26,19 +26,14 @@ if(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='patient') {
      die("Connection Failed : ". $conn->connect_error);
 	 }else { 
 
-      $old_email= $_SESSION["email"];
-	    $old_name=$_SESSION["name"];
-      $old_password=$_SESSION["password"];
-	    $old_bday=$_SESSION["bday"];
-      $old_phone=$_SESSION["phone"];
       if (isset($_SESSION["location"])){
       $old_location=$_SESSION["location"];
       }
       /*if (isset($_SESSION["pf_img"])){
       $old_pf_img = $_SESSION["pf_img"];
       }*/
-
 		  $old_pf_img = "assets/pfp2.png";
+      
 
       if ($_SERVER["REQUEST_METHOD"] === "POST") {
         
@@ -55,7 +50,7 @@ if(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='patient') {
           $stmt->bind_param("ss", $new_email , $new_email);
           $stmt->execute();
           $result = $stmt->get_result();
-          if ( $new_email != $old_email  &&  $result->num_rows > 0) {
+          if ( $new_email != $_SESSION["email"]  &&  $result->num_rows > 0) {
             echo "email already exists";
 
         }else{
@@ -65,20 +60,20 @@ if(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='patient') {
           $_SESSION["email"]= $new_email;
         }       
 			}
-         if( $new_name  && $new_name!=$old_name ){
+         if( $new_name  && $new_name!=$_SESSION["name"] ){
           $stmt = $conn->prepare("UPDATE patient SET name = ? WHERE id = ?");
           $stmt->bind_param("si", $new_name, $_SESSION["id"]);
           $stmt->execute();
           $_SESSION["name"]=$new_name;
          }
 
-         if($new_phone && $new_phone!=$old_phone ){
+         if($new_phone && $new_phone!=$_SESSION["phone"] ){
           $stmt = $conn->prepare("UPDATE patient SET phone = ? WHERE id = ?");
           $stmt->bind_param("si", $new_phone, $_SESSION["id"]);
           $stmt->execute();
           $_SESSION["phone"]=$new_phone;
          }
-          if($new_bday  && $new_bday!=$old_bday ){
+          if($new_bday  && $new_bday!=$_SESSION["bday"] ){
           $stmt = $conn->prepare("UPDATE patient SET bday = ? WHERE id = ?");
           $stmt->bind_param("si", $new_bday, $_SESSION["id"]);
           $stmt->execute();
@@ -90,8 +85,8 @@ if(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='patient') {
           $stmt->execute();
           $_SESSION["location"]=$new_location;
          }
-         if ($new_password && $confirm_password && !password_verify($new_password ,$old_password)) {
-             if(password_verify($confirm_password,$old_password)) {
+         if ($new_password && $confirm_password && !password_verify($new_password ,$_SESSION["password"])) {
+             if(password_verify($confirm_password,$_SESSION["password"])) {
                 $password_hashed = password_hash($new_password, PASSWORD_DEFAULT);
                 $stmt = $conn->prepare("UPDATE patient SET password = ? WHERE id = ?");
                 $stmt->bind_param("si",$password_hashed , $_SESSION["id"]);
@@ -123,15 +118,15 @@ if(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='patient') {
 	<div class="pf_header">
 		<img src="<?php echo $old_pf_img ?>"/>
 		<div class="pf_header_text">
-    <div class="pf_body_field"><h3>Nom</h3><input type="text" value="<?php echo $old_name ?>" name="name" autocomplete="off"/></div>
+    <div class="pf_body_field"><h3>Nom</h3><input type="text" value="<?php echo $_SESSION["name"] ?>" name="name" autocomplete="off"/></div>
 		</div>
 	</div>
 	<div class="pf_body">
-  <div class="pf_body_field"><h3>Email</h3><input class="in_text" type="text"value="<?php echo $old_email ?>" name="email" autocomplete="off"/></div>
+  <div class="pf_body_field"><h3>Email</h3><input class="in_text" type="text"value="<?php echo $_SESSION["email"] ?>" name="email" autocomplete="off"/></div>
 		<div class="pf_body_field"><h3>Date Naissance</h3><input type="date" name="bday"></div>
 	</div>
 	<div>
-  <div class="pf_body_field"><h3>Numero telephone</h3><input type="text" value="<?php echo $old_phone ?>" name="phone"  autocomplete="off"/></div>
+  <div class="pf_body_field"><h3>Numero telephone</h3><input type="text" value="<?php echo $_SESSION["phone"] ?>" name="phone"  autocomplete="off"/></div>
   <div class="pf_body_field"><h3>Location</h3><input type="text" value="<?php echo $old_location ?>" name="location" autocomplete="off" /></div>
 		<div class="pf_body_field"><h3>Password</h3><input class="in_text" type="password" placeholder="enter old password" name="old_password" autocomplete="off">
     <input class="in_text" type="password" placeholder="enter new password" name="new_password" autocomplete="off" ></div>
