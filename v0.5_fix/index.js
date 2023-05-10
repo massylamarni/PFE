111457d7f123367574afacba936b20fa5909042a
massylamarni <massyl.amarni@outlook.fr>
2023-05-10 11:19:34 +0100

    modified:   v0.5_fix/index.js

--- v0.5_fix/index.js
+++ v0.5_fix/index.js
@@ -87,6 +87,224 @@ function bookform(op, doctor_id)
 	+ date_display.date + " " + date_display.month + " a " + date_display.time + "\nMotif: " + motif_in;
 }
 
+
+//funtion for the size of texts area
+function sizeArea(){let maxTextArea=[50,15,15,500,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15];
+	let el=document.getElementsByTagName('textarea');
+	for (i=0; i<el.length; i++){
+		let line = el[i].value.split('\n');
+		let row = line.length;
+		let col = line.reduce(function(max, line){
+			return Math.max(max, line.length);
+		}, 0);
+		el[i].rows=row;
+		el[i].cols=col;
+	}
+};
+
+var el=document.getElementsByTagName('textarea');
+for (let k=0;el.length;k++){
+el[k].addEventListener('keydown',function(event){
+		if(el[k].value.length<=maxTextArea[k]){
+			sizeArea();
+		}else{
+			event.preventDefault();
+			el[k].value = el[k].value.slice(0,-1);
+		}
+	}
+)
+}
+/*
+// ajouter une langue
+var ajouter=document.getElementById('ajouter');
+
+let langue=document.getElementById('prelangue')
+
+ajouter.addEventListener('click',function(event){
+	event.preventDefault();
+	let nouvel=document.createElement('textarea');
+	nouvel.after(ajouter)
+	nouvel.rows=1;
+	nouvel.cols=15;
+	/*nouvel.addEventListener('keydown',function(event){
+		if(nouvel.value.length <= 15){
+			sizeArea();
+		}else{event.preventDefault();
+			nouvel.value = nouvel.value.slice(0,-1);}
+	})
+	nouvel.classList.add('tarifdiv');
+	langue.appendChild(nouvel);
+});
+*/
+
+//ajouter diplome et classification
+var ajouterdiplome=document.getElementById('diplome');
+
+let diplome=document.getElementById('prediplome')
+
+ajouterdiplome.addEventListener('click',function(event){
+	event.preventDefault();
+	let nouvel=document.createElement('textarea');
+	let annee=document.createElement('textarea');
+	nouvel.after(ajouterdiplome);
+	annee.after(diplome);
+	nouvel.rows=1;
+	nouvel.cols=50;
+	annee.rows=1;
+	annee.cols=10;
+	annee.classList.add('pf_body_field');
+	nouvel.classList.add('pf_body_field');
+/*	nouvel.addEventListener('keydown',function(event){
+		if(nouvel.value.length <= 15){
+			sizeArea();
+		}else{event.preventDefault();
+			nouvel.value = nouvel.value.slice(0,-1);}
+	})
+	annee.addEventListener('keydown',function(event){
+		if(annee.value.length <= 15){
+			sizeArea();
+		}else{event.preventDefault();
+			annee.value = nouvel.value.slice(0,-1);}
+	})*/
+
+	let container=document.createElement('div');
+	  container.appendChild(annee);
+	  container.appendChild(nouvel);
+	  container.classList.add('tarifdiv');
+	diplome.appendChild(container);
 	
+
+});
+
+//ajouter tarifs
+let ajoutertarifs=document.getElementById('tarif');
+
+let tarifs=document.getElementById('pretarif');
+
+ajoutertarifs.addEventListener('click',function(event){
+	event.preventDefault();
+	let nouvel=document.createElement('textarea');
+	let prix=document.createElement('textarea');
+	nouvel.after(ajoutertarifs);
+	prix.after(ajoutertarifs);
+	nouvel.rows=1;
+	nouvel.cols=50;
+	prix.rows=1;
+	prix.cols=10;
+	prix.classList.add('pf_body_field');
+	nouvel.classList.add('pf_body_field');
+	prix.before(ajoutertarifs);
+	nouvel.after(ajoutertarifs);
+/*	nouvel.addEventListener('keydown',function(event){
+		if(nouvel.value.length <= 15){
+			sizeArea();
+		}else{event.preventDefault();
+			nouvel.value = nouvel.value.slice(0,-1);}
+	})
+	prix.addEventListener('keydown',function(event){
+		if(prix.value.length <= 15){
+			sizeArea();
+		}else{event.preventDefault();
+			prix.value = nouvel.value.slice(0,-1);}
+	})*/
+	let container=document.createElement('div');
+	  container.appendChild(nouvel);
+	  container.appendChild(prix);
+	  container.classList.add('tarifdiv');
+	tarifs.appendChild(container);
+	
+
+});
+
+//ajouter un rendez-vous
+function ajouterunrendez(){
+let ajouterrendez=document.getElementById('ajouterrendez');
+
+ajouterrendez.addEventListener('click',function(event){
+	event.preventDefault();
+    let photo=document.createElement('img');
+	photo.src='assets/pfp2.png';
+	
+
+	let nom=document.createElement('input');
+	nom.type='text';
+    nom.classList.add('pfp_text_name');
+	nom.style.width='128px';
+
+    let pfp_text=document.createElement('div');
+	pfp_text.classList.add('pfp_text');
+    pfp_text.appendChild(nom);
+
+	let pfp =document.createElement('div');
+	pfp.appendChild(photo);
+	pfp.appendChild(pfp_text);
+    pfp.classList.add('pfp');
+
+	let calend=document.createElement('input');
+	calend.type='datetime-local';
+	calend.classList.add('brief_datetime');
+	calend.style.width='100px';
+	calend.style.height='20px';
+	calend.style.marginTop='37px';
+	calend.style.padding='20px;'
+
+	let divtime=document.createElement('div');
+	divtime.classList.add('brief_datetime');
+
+	let motif=document.createElement('p');
+	let motif2=document.createTextNode('Motife de consultation');
+	motif.appendChild(motif2);
+	motif.classList.add('brief_motif');
+	motif.addEventListener('mouseover',function(event){
+		event.preventDefault();
+		motif.style.cursor='pointer';
+		motif.style.textDecoration='underline';
+	});
+	motif.addEventListener('mouseout',function(event){
+		event.preventDefault();
+		motif.style.textDecoration='none';
+	})
+
+	let ajouter=document.createElement('p');
+	let ajouter2=document.createTextNode('ajouter');
+	ajouter.appendChild(ajouter2);
+	ajouter.classList.add('brief_cancel');
+	ajouter.addEventListener('mouseover',function(event){
+		event.preventDefault();
+		ajouter.style.cursor='pointer';
+		ajouter.style.textDecoration='underline';
+
+	});
+	ajouter.addEventListener('mouseout',function(event){
+		event.preventDefault();
+		ajouter.style.textDecoration='none';
+	})
+
+	let list_el_brief=document.createElement('div');
+	list_el_brief.appendChild(calend);
+	list_el_brief.appendChild(divtime);
+	list_el_brief.appendChild(motif);
+	list_el_brief.appendChild(ajouter);
+	list_el_brief.classList.add('list_el_brief');
+	
+
+	let tout=document.createElement('div');
+	tout.appendChild(pfp);
+	tout.appendChild(list_el_brief);
+    tout.classList.add('list_el');
+	
+	
+
+	
+
+	let list=document.getElementsByClassName('list')[0];
+	list.appendChild(tout);
+
+	let generale=document.getElementsByClassName('std_containerI')[0];
+	generale.appendChild(list);
+})};
+
+
+
 	
 
