bb591e70312dcb459f032004dcc9b01b5da61cb0
YacineAitKaci <yacinej86@gmail.com>
2023-05-13 16:16:34 +0100

    modified:   v0.5_fix/doctor_signup.php 	modified:   v0.5_fix/login.php 	modified:   v0.5_fix/patient_signup.php

--- v0.5_fix/login.php
+++ v0.5_fix/login.php
@@ -13,8 +13,18 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+if(!isset($_SESSION)){session_start();}
 
-if(!isset($_SESSION["usertype"]))  {   
+if(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='doctor'){
+
+	header("Location: doctor_index.php");
+	exit();
+	
+	}elseif(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='patient'){
+	
+	  header("Location: patient_index.php");
+	  exit();
+	}  
 
 $conn = mysqli_connect('localhost','root','','Client');
 
@@ -134,13 +144,3 @@ $conn->close();
 
 </body>
 </html>
-<?php   }elseif(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='doctor'){
-
-header("Location: doctor_index.php");
-exit();
-
-}elseif(isset($_SESSION["usertype"]) && $_SESSION["usertype"]=='patient'){
-
-  header("Location: patient_index.php");
-  exit();
-}  ?>
