19460f6778c8abac45faa9821d2c8c3937686904
YacineAitKaci <yacinej86@gmail.com>
2023-05-26 16:56:12 +0100

    new file:   v0.5_fix/admin.sql 	new file:   v0.5_fix/admin/admin_dashboard.php 	new file:   v0.5_fix/admin/doctor_add.php 	new file:   v0.5_fix/admin/mod_dashboard.php 	new file:   v0.5_fix/admin/patient_add.php 	new file:   v0.5_fix/admin/search_account.php 	new file:   v0.5_fix/admin/style.css

---
+++ v0.5_fix/admin/mod_dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="form-container">
+<h3>Patient Ajout </h3>
+<?php include("patient_add.php");?>
+<br><br><h3>Doctor Ajout </h3><br><br>
+<?php include("doctor_add.php");?>
+<br><br><h3>recherche de compte </h3><br><br>
+<?php include("search_account.php");?>
+</div>
+</body>
+</html>
